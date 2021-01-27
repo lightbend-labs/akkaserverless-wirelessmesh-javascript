d610a03de97aa806c1a1574b279887c769cda304
sean-walsh <t3chi3@msn.com>
2021-01-27 12:57:59 -0500

    Working unit tests.

--- wirelessmesh/test/test.js
+++ wirelessmesh/test/test.js
@@ -13,41 +13,14 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const path = require("path");
 const should = require("chai").should();
-const grpc = require("grpc");
-const protoLoader = require("@grpc/proto-loader");
-const protobuf = require("protobufjs");
-const protobufHelper = require("cloudstate/src/protobuf-helper");
-const AnySupport = require("cloudstate/src/protobuf-any");
-
-const allIncludeDirs = protobufHelper.moduleIncludeDirs.concat([
-  path.join(__dirname, "..", "node_modules", "cloudstate", "proto")
-]);
-
-const packageDefinition = protoLoader.loadSync(
-    [
-      path.join( __dirname, "..", "node_modules", "cloudstate" ,"proto", "cloudstate", "entity.proto"),
-      path.join( __dirname, "..", "node_modules", "cloudstate" ,"proto", "cloudstate", "event_sourced.proto")
-    ],
-    {
-      //includeDirs: allIncludeDirs
-    });
-const descriptor = grpc.loadPackageDefinition(packageDefinition);
-
-const root = protobufHelper.loadSync([
-  path.join( __dirname, "..", "wirelessmeshservice.proto")
-], allIncludeDirs);
-const anySupport = new AnySupport(root)
+const entity = require('../wirelessmesh');
 
 // Customer location attributes under test
 const customerLocationId = "customerLocationId1";
 const accessToken = "someaccesstoken";
 const deviceId = "deviceId1";
 const room = "living room";
-const {addCustomerLocation} = require("../wirelessmesh");
-
-//const customerLocation = require('../wirelessmesh');
 
 function mockContext() {
     return {
@@ -59,30 +32,227 @@ function mockContext() {
         emit(event) {
             this.events.push(event);
         },
+        reset() {
+          this.events = [];
+          this.failures = [];
+        }
     };
+}
+
+function defaultState() {
+  return {
+    added: false,
+    removed: false,
+    accessToken: "",
+    devices: []
+  };
 }
 
 describe("customer location", () => {
 
   it("should create a customer location", () => {
-
-    const entityState = {
-      added: false,
-      removed: false,
-      accessToken: "",
-      devices: []
-    };
-
+    const state = defaultState();
     const context = mockContext();
 
-    const response = addCustomerLocation(
-        {customerLocationId: customerLocationId, accessToken: accessToken},
-        entityState,
-        context);
+    entity.addCustomerLocation(
+    {customerLocationId: customerLocationId, accessToken: accessToken},
+      state,
+    context);
 
+    context.failures.length.should.equal(0);
     context.events.length.should.equal(1);
     context.events[0].type.should.equal("CustomerLocationAdded")
     context.events[0].customerLocationId.should.equal(customerLocationId);
     context.events[0].accessToken.should.equal(accessToken);
+
+    // Send the event and check state is updated.
+    entity.customerLocationAdded(context.events[0], state);
+    state.added.should.equal(true);
+    state.removed.should.equal(false);
+    state.accessToken.should.equal(accessToken);
   });
+
+  it("should remove a customer location", () => {
+    const state = defaultState();
+    const context = mockContext();
+    entity.addCustomerLocation(
+     {customerLocationId: customerLocationId, accessToken: accessToken},
+     state,
+     context);
+    entity.customerLocationAdded(context.events[0], state);
+
+    context.reset();
+    entity.removeCustomerLocation(
+     {customerLocationId: customerLocationId},
+      state,
+      context);
+
+    entity.customerLocationRemoved(context.events[0], state);
+    context.failures.length.should.equal(0);
+    state.removed.should.equal(true);
+    context.events[0].type.should.equal("CustomerLocationRemoved")
+    context.events[0].customerLocationId.should.equal(customerLocationId);
+
+    const response = entity.getCustomerLocation(
+      {customerLocationId: customerLocationId},
+       state,
+       context);
+
+    context.failures.length.should.equal(1);
+    context.failures[0].should.equal("customerLocation does not exist")
+  });
+
+  it("should activate a device", () => {
+    const state = defaultState();
+    const context = mockContext();
+    entity.addCustomerLocation(
+      {customerLocationId: customerLocationId, accessToken: accessToken},
+      state,
+      context);
+    entity.customerLocationAdded(context.events[0], state);
+
+    context.reset();
+    entity.activateDevice({customerLocationId: customerLocationId, deviceId, deviceId},
+    state,
+    context)
+
+    context.failures.length.should.equal(0);
+    context.events.length.should.equal(1);
+    context.events[0].type.should.equal("DeviceActivated")
+    context.events[0].customerLocationId.should.equal(customerLocationId);
+    context.events[0].deviceId.should.equal(deviceId);
+
+    entity.deviceActivated(context.events[0], state);
+    state.devices.length.should.equal(1);
+    state.devices[0].customerLocationId.should.equal(customerLocationId);
+    state.devices[0].deviceId.should.equal(deviceId);
+    state.devices[0].room.should.equal("");
+    state.devices[0].nightlightOn.should.equal(false);
+  });
+
+  it("should remove a device", () => {
+    const state = defaultState();
+    const context = mockContext();
+    entity.addCustomerLocation(
+      {customerLocationId: customerLocationId, accessToken: accessToken},
+      state,
+      context);
+    entity.customerLocationAdded(context.events[0], state);
+    context.reset();
+    entity.activateDevice({customerLocationId: customerLocationId, deviceId, deviceId},
+      state,
+      context)
+    entity.deviceActivated(context.events[0], state);
+
+    context.reset();
+    entity.removeDevice(
+      {customerLocationId: customerLocationId, deviceId: deviceId},
+      state,
+      context);
+
+    context.failures.length.should.equal(0);
+    context.events.length.should.equal(1);
+    context.events[0].type.should.equal("DeviceRemoved")
+    context.events[0].customerLocationId.should.equal(customerLocationId);
+    context.events[0].deviceId.should.equal(deviceId);
+
+    entity.deviceRemoved(context.events[0], state)
+    state.devices.length.should.equal(0);
+  });
+
+  it("should assign a room to a device", () => {
+    const state = defaultState();
+    const context = mockContext();
+    entity.addCustomerLocation(
+      {customerLocationId: customerLocationId, accessToken: accessToken},
+      state,
+      context);
+    entity.customerLocationAdded(context.events[0], state);
+    context.reset();
+    entity.activateDevice({customerLocationId: customerLocationId, deviceId, deviceId},
+      state,
+      context)
+    entity.deviceActivated(context.events[0], state);
+
+    context.reset();
+    entity.assignRoom(
+      {customerLocationId: customerLocationId, deviceId: deviceId, room: room},
+      state,
+      context);
+
+    context.failures.length.should.equal(0);
+    context.events.length.should.equal(1);
+    context.events[0].type.should.equal("RoomAssigned")
+    context.events[0].customerLocationId.should.equal(customerLocationId);
+    context.events[0].deviceId.should.equal(deviceId);
+    context.events[0].room.should.equal(room);
+
+    entity.roomAssigned(context.events[0], state)
+    state.devices.length.should.equal(1);
+    state.devices[0].room.should.equal(room);
+  });
+
+  it("toggle a device nightlight", () => {
+    const state = defaultState();
+    const context = mockContext();
+    entity.addCustomerLocation(
+      {customerLocationId: customerLocationId, accessToken: accessToken},
+      state,
+      context);
+    entity.customerLocationAdded(context.events[0], state);
+    context.reset();
+    entity.activateDevice({customerLocationId: customerLocationId, deviceId, deviceId},
+      state,
+      context)
+    entity.deviceActivated(context.events[0], state);
+
+    context.reset();
+    entity.toggleNightlight(
+      {customerLocationId: customerLocationId, deviceId: deviceId},
+      state,
+      context);
+
+    context.failures.length.should.equal(0);
+    context.events.length.should.equal(1);
+    context.events[0].type.should.equal("NightlightToggled")
+    context.events[0].customerLocationId.should.equal(customerLocationId);
+    context.events[0].deviceId.should.equal(deviceId);
+    context.events[0].nightlightOn.should.equal(true);
+
+    entity.nightlightToggled(context.events[0], state)
+    state.devices.length.should.equal(1);
+    state.devices[0].nightlightOn.should.equal(true);
+  });
+
+  it("should get a customer location", () => {
+    const state = defaultState();
+    const context = mockContext();
+    entity.addCustomerLocation(
+      {customerLocationId: customerLocationId, accessToken: accessToken},
+      state,
+      context);
+    entity.customerLocationAdded(context.events[0], state);
+    context.reset();
+    entity.activateDevice({customerLocationId: customerLocationId, deviceId, deviceId},
+      state,
+      context)
+    entity.deviceActivated(context.events[0], state);
+
+    const response = entity.getCustomerLocation(
+      {customerLocationId: customerLocationId},
+      state,
+      context);
+
+    context.failures.length.should.equal(0);
+    response.added.should.equal(true);
+    response.removed.should.equal(false);
+    response.devices.length.should.equal(1);
+    response.accessToken.should.equal(accessToken);
+    response.devices[0].customerLocationId.should.equal(customerLocationId);
+    response.devices[0].deviceId.should.equal(deviceId);
+    response.devices[0].activated.should.equal(true);
+    response.devices[0].room.should.equal("");
+    response.devices[0].nightlightOn.should.equal(false);
+  });
+
 });
